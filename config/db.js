4f2f2f6afa68a0759c7b77a22b10c25145b34876
Centman7089 <innocentimitini@gmail.com>
2025-07-23 12:46:19 +0100

    updated

--- config/db.js
+++ config/db.js
@@ -2,8 +2,10 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+  try
+  {
+    const dbUrl = process.env.MONGO_URI || 'mongodb+srv://pageinnovations:UJx4sO99Ri6dok8m@praise.pu51dsk.mongodb.net/Praise?retryWrites=true&w=majority&appName=Praise'
+    const conn = await mongoose.connect(dbUrl);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
